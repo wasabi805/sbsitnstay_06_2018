ece60f38156267d4c69b04416121739d31790282
‘Tim <timothy.j.ocampo@gmail.com>
2018-06-06 10:29:32 -0700

    see ln 10 && 11 in validation/register.js : use of ternary : can either be user form input of blank '' : if no form input, field will be '' and then fail validation and the errors {} gets filled with the appropriate error msg

--- validation/register.js
+++ validation/register.js
@@ -6,8 +6,24 @@ module.exports = function validateRegisterInput(data) {
 
     let errors = {};
 
+    //set to string so it can fail custom validation func
+    data.firstName = !isEmpty(data.firstName) ? data.firstName : '';
+    data.lastName = !isEmpty(data.lastName) ? data.lastName : '';
+
     if(!Validator.isLength(data.firstName, {min: 2, max: 30})){
-        errors.firstName = 'Name must be between 2 and 30 characters'
+        errors.firstName = 'First name must be between 2 and 30 characters';
+    }
+
+    if(!Validator.isLength(data.lastName, {min: 2, max: 30})){
+        errors.lastName = 'Last name must be between 2 and 30 characters';
+    }
+
+    if(Validator.isEmpty(data.firstName)){
+        errors.firstName = "First name field is required";
+    }
+
+    if(Validator.isEmpty(data.lastName)){
+        errors.firstName = "Last name field is required";
     }
 
     return{
