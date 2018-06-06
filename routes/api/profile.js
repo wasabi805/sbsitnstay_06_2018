6342ec84a08e111616b1ea908a8092909d77b683
‘Tim <timothy.j.ocampo@gmail.com>
2018-06-06 15:51:28 -0700

    Find user profile by user_id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -75,6 +75,34 @@ router.get('/handle/:handle', (req,res)=>{
 });
 
 
+//  -----   @prefix routes/api/profile -----
+
+//@route    GET api/profile/user/:user_id
+//@desc     Retrieve user profile by user ID
+//@access   PUBLIC
+
+router.get('/user/:user_id', (req,res)=>{
+
+    const {errors} = {};
+
+    Profile.findOne({user: req.params.user_id})
+        .populate({
+            model: 'users',
+            path: 'user',
+            select: ['firstName', 'lastName', 'email', 'phone']
+        }).then(profile =>{
+        //if no profile found
+        if(!profile){
+            errors.noprofile = "This user hasn't set up a profile";
+            res.status(404).json(errors);
+        }
+
+        res.json(profile);
+    })
+        .catch(err=> res.status(404).json(err))
+});
+
+
 
 //  -----   @prefix routes/api/profile -----
 
@@ -135,7 +163,7 @@ router.post('/', passport.authenticate('jwt', {session: false}), (req,res)=>{
                             errors.handle = 'That Handle already exists';
                             res.status(400).json(errors);
                         }
-                    })
+                    });
                 //Save Profile
                 new Profile(profileFields).save()
                     .then(profile=> res.json(profile));
