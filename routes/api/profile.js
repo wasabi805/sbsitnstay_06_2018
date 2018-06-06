56a70aeb1a0f44cd05794be794e438e118f4b8b0
‘Tim <timothy.j.ocampo@gmail.com>
2018-06-06 13:17:52 -0700

    starting on route for already logged in current user to create a profile: this route initializes an obj called profileFields: the user is passed into this obj via their user id that already lives in the req since we are already logged in an authenticated. : an if stmnt checks if the handle originiated from the form submit, if this is true, then inside the profileFields obj, we'll create a key of handle and set it's value === form field submit for handle.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -13,7 +13,7 @@ router.get('/test', (req,res)=>res.json({msg: ' /routes/api/profile/test is work
 
 //  -----   @prefix routes/api/profile -----
 
-//@route    Get api/profile
+//@route    GET api/profile
 //@desc     Gets the current users profile
 //@access   PRIVATE
 
@@ -36,6 +36,31 @@ router.get('/', passport.authenticate('jwt', {session: false}), (req,res)=>{
         res.json(profile);
 
     }).catch(err => res.status(404).json(err));
+});
+
+
+
+//  -----   @prefix routes/api/profile -----
+
+//@route    POST api/profile
+//@desc     Create a user profile
+//@access   PRIVATE
+
+router.post('/', passport.authenticate('jwt', {session: false}), (req,res)=>{
+//IMPORTANT! recall upon login success we received a token: that token inserted the user into req.user
+    // req.user.id
+
+    const errors = {};
+
+    //Get Fields
+    const profileFields = {};
+    profileFields.user = req.user.id;
+
+    if(req.body.handle){
+        //checks if the data was actually sent from the form: if true add the handle from post to profileFields {}
+        profileFields.handle = req.body.handle;
+    }
+
 });
 
 
