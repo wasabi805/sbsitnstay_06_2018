ffd930584061e60617bdffa0dfcc6092bc109ec8
‘Tim <timothy.j.ocampo@gmail.com>
2018-06-08 16:38:45 -0700

    Rearranged Comps ==> All comps get fed to App.js for now ALSO : pushing before setting up react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
 import Header from './components/layout/Header';
+import Section from './components/layout/Section'
+import Elements from './components/layout/Elements';
+import Footer from './components/layout/Footer';
 
 import Landing from './components/layout/Landing';
 
-import './assets/css/App.scss';
+require("./assets/css/App.css");
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      {/*<h1>Hello World</h1>*/}
-        <Landing />
-        {/*<Header/>*/}
+        
+          <div id='wrapper'>
+              <Header/>
+              <Section/>
+              <Elements/>
+              <Footer/>
+              <Landing />
+          </div>
+
+          {/*BACKGROUND*/}
+          <div id="bg"></div>
+
+          {/*Scripts*/}
+          <script src="assets/js/jquery.min.js"></script>
+          <script src="assets/js/skel.min.js"></script>
+          <script src="assets/js/util.js"></script>
+          <script src="assets/js/main.js"></script>
 
       </div>
     );
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,31 +1,9 @@
 import React, {Component} from 'react';
-import Header from './Header'
-import Section from './Section'
-import Elements from './Elements';
-import Footer from './Footer'
-
-require("../../assets/css/App.css");
 
 class Landing  extends Component{
     render(){
         return(
             <div>
-
-               <div id='wrapper'>
-                   <Header/>
-                   <Section/>
-                   <Elements/>
-                    <Footer/>
-               </div>
-
-                {/*BACKGROUND*/}
-                <div id="bg"></div>
-
-                {/*Scripts*/}
-                <script src="assets/js/jquery.min.js"></script>
-                <script src="assets/js/skel.min.js"></script>
-                <script src="assets/js/util.js"></script>
-                <script src="assets/js/main.js"></script>
 
             </div>
         )
